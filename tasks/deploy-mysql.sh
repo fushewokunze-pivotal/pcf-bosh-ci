c82aa71d4ed92bf5b4079133c7a7428525f476eb
Fadzi Ushewokunze <fushewokunze@pivotal.io>
2017-03-13 18:01:32 -0400

    Update deploy-mysql.sh

--- tasks/deploy-mysql.sh
+++ tasks/deploy-mysql.sh
@@ -24,14 +24,14 @@ mysql_monitoring_cluster_identifier: "p-mysql-$(cat terraform-state/name)"
 ########################
 
 cf_api_url: https://api.$(jq -r .sys_domain terraform-state/metadata)
-cf_uaa_admin_client_secret: $(bosh int cf-vars-store/*-cf-vars-store.yml --path /uaa_admin_client_secret)
-cf_admin_username: admin
-cf_admin_password: $(bosh int cf-vars-store/*-cf-vars-store.yml --path /uaa_scim_users_admin_password)
+cf_uaa_admin_client_secret: ((apps_manager_client_password))
+cf_admin_username: push_apps_manager
+cf_admin_password: ((push_apps_manager_password))
 cf_app_domains: [$(jq -r .apps_domain terraform-state/metadata)]
 cf_sys_domain: $(jq -r .sys_domain terraform-state/metadata)
 cf_skip_ssl_validation: true
 nats:
-  password: $(bosh int cf-vars-store/*-cf-vars-store.yml --path /nats_password)
+  password: ((nats_password))
   machines: ((nats_ips))
   user: nats
   port: 4222
